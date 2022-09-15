7f6dab12b3f986257294a194869445faa1d5d3b0
bciobo <bogdan.ciobotaru@linus.finance.com>
2022-09-15 11:36:15 +0200

    add functions to flatten names in departments.json

--- app/app/services/utils.py
+++ app/app/services/utils.py
@@ -1,6 +1,7 @@
 import json
 import os
 import typing as t
+from difflib import SequenceMatcher
 
 from app import config
 
@@ -9,3 +10,9 @@ def read_departments_json() -> t.List[t.Dict[str, t.Any]]:
     """Parse 'departments.json' as dict."""
     with open(os.path.join(config.PARENT_DIR, config.DEPARTMENTS_JSON_FILENAME)) as f:
         return json.load(f).get('departments')
+
+
+def calculate_string_similarity(str_1: str, str_2: str) -> int:
+    """Calculate similarity ratio of given strings as a rounded percentage."""
+    s = SequenceMatcher(None, str_1, str_2)
+    return int(round(s.ratio(), 2) * 100)
