5ea525f58faeb55993c9fc3d285de885f81b90bc
bciobo <bogdan.ciobotaru@linus.finance.com>
2022-09-15 11:29:52 +0200

    add function to read departments.json

--- app/app/config.py
+++ app/app/config.py
@@ -1,1 +1,5 @@
-API_PREFIX = '/api'
+import os
+
+PARENT_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), '..', '..'))
+TEMPLATES_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), 'templates'))
+DEPARTMENTS_JSON_FILENAME = 'departments.json'
